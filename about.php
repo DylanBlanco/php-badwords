c7506643d1f5cd3ad886bf7e85b7ce0463db8639
DylanBlanco <dylanblanco1198@gmail.com>
2024-09-26 16:51:32 +0200

    aggiunto collegamento tra index e about

--- about.php
+++ about.php
@@ -7,10 +7,19 @@
     <!--link css-->
     <link rel="stylesheet" href="css/style.css">
     
-    <title>Document</title>
+    <title>Badwords-about</title>
 </head>
 <body>
-    
+    <header>
+        <div class="container">
+            <h1>Badwords</h1>
+        </div>
+    </header>
+    <main>
+        <div class="container">
+            <a href="./index.php">Torna alla home</a>
+        </div>
+    </main>
     <!--link js-->
     <script type="text/javascript" src="js/script.js"></script>
 </body>
---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--link css-->
+    <link rel="stylesheet" href="css/style.css">
+    
+    <title>Badwords</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <h1>Badwords</h1>
+        </div>
+    </header>
+    <main>
+        <div class="container">
+            <form action="./about.php">
+                <div>
+                    <input
+                    type="text"
+                    placeholder="nome">
+                </div>
+                <div>
+                    <button
+                    type="submit">Invia</button>
+                </div>
+            </form>
+        </div>
+    </main>
+    <!--link js-->
+    <script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
